63a11db1a088346af9433f695f7217562f274477
devsakae <devsakae@gmail.com>
2022-10-25 09:47:22 -0300

    Footer codado e testado 100% funcionando

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Profile() {
   return (
     <div>
       <Header />
+      <p>Sou o Profile</p>
+      <Footer />
     </div>
   );
 }
--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,29 +1,48 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Footer from '../components/Footer';
-import renderWithRouter from './renderWith';
+import { renderWithRouter } from './renderWith';
+import RecipesProvider from '../context/RecipesProvider';
+import Meals from '../pages/Meals';
 
 describe('Testes da tela Footer - Menu inferior', () => {
   it('Procura elementos obrigatórios', () => {
-    render(<Footer />);
-    expect(screen.getByTestId('footer')).toBeInTheDocument();
-    expect(screen.getByTestId('drinks-bottom-btn')).toBeInTheDocument();
-    expect(screen.getByTestId('meals-bottom-btn')).toBeInTheDocument();
+    const { history } = renderWithRouter(
+      <RecipesProvider>
+        <Meals />
+      </RecipesProvider>,
+    );
+    if (history.location.pathname === '/meals') {
+      expect(screen.getByTestId('footer')).toBeInTheDocument();
+      expect(screen.getByTestId('drinks-bottom-btn')).toBeInTheDocument();
+      expect(screen.getByTestId('meals-bottom-btn')).toBeInTheDocument();
+    }
   });
   it('Verifica se é possível ir para a rota /drinks', () => {
-    renderWithRouter(<Footer />);
-    const drinksBtn = screen.getByTestId('drinks-bottom-btn');
-    userEvent.click(drinksBtn);
-    const location = useLocation();
-    expect(location.pathname).toBe('/drinks');
+    const { history } = renderWithRouter(
+      <RecipesProvider>
+        <Meals />
+      </RecipesProvider>,
+    );
+    if (history.location.pathname === '/meals') {
+      const drinksBtn = screen.getByTestId('drinks-bottom-btn');
+      userEvent.click(drinksBtn);
+      const location = useLocation();
+      expect(location.pathname).toBe('/drinks');
+    }
   });
   it('Verifica se é possível ir para a rota /drinks', () => {
-    renderWithRouter(<Footer />);
-    const mealsBtn = screen.getByTestId('meals-bottom-btn');
-    userEvent.click(mealsBtn);
-    const location = useLocation();
-    expect(location.pathname).toBe('/meals');
+    const { history } = renderWithRouter(
+      <RecipesProvider>
+        <Meals />
+      </RecipesProvider>,
+    );
+    if (history.location.pathname === '/meals') {
+      const mealsBtn = screen.getByTestId('meals-bottom-btn');
+      userEvent.click(mealsBtn);
+      const location = useLocation();
+      expect(location.pathname).toBe('/meals');
+    }
   });
 });
