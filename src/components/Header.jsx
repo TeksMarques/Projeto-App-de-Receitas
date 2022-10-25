7dfeb54c98ef4b86adbb72b86c3aba1b63a3636e
Teresa Marques <taotaodoce@gmail.com>
2022-10-24 22:01:54 -0300

    testes do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,6 +24,7 @@ export default function Header() {
       && (
         <button
           type="button"
+          data-testid="search-input"
           onClick={ showSearch }
         >
           <img
---
+++ src/tests/Header.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Meals from '../pages/Meals';
+import { renderWithRouter } from './renderWith';
+import RecipesProvider from '../context/RecipesProvider';
+
+test('Testa botao de perfil', () => {
+  const { history } = renderWithRouter(
+    <RecipesProvider>
+      <Meals />
+    </RecipesProvider>,
+  );
+  if (history.location.pathname === '/meals') {
+    const profileLink = screen.getByTestId('profile-top-btn');
+    expect(profileLink).toBeInTheDocument();
+    userEvent.click(profileLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/profile');
+  }
+});
+test('Testa botão de pesquisa', () => {
+  const { history } = renderWithRouter(
+    <RecipesProvider>
+      <Meals />
+    </RecipesProvider>,
+  );
+  if (history.location.pathname === '/meals') {
+    const profileLink = screen.getByTestId('profile-top-btn');
+    const searchLink = screen.getByRole('img', {
+      name: /serach icon/i,
+    });
+    expect(profileLink).toBeInTheDocument();
+    expect(searchLink).toBeInTheDocument();
+    userEvent.click(searchLink);
+    const searchInput = screen.getByTestId('search-input');
+    expect(searchInput).toBeInTheDocument();
+  }
+});
