8a0d2659847d5d8e91adceedb57ddc8f26c057bf
devsakae <devsakae@gmail.com>
2022-10-25 15:30:17 -0300

    Erro de digitação corrigido

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -15,13 +15,12 @@ const fetchMealBy = async (modo, valor) => {
 
 const fetchDrinkBy = async (modo, valor) => {
   let endpoint = '';
-  if (modo === 'byIngredients') { endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i==${valor}`; }
+  if (modo === 'byIngredients') { endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${valor}`; }
   if (modo === 'byName') { endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${valor}`; }
   if (modo === 'byFirstLetter') { endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${valor}`; }
   try {
     const response = await fetch(endpoint);
     const responseJson = await response.json();
-    console.log(responseJson);
     return responseJson.drinks;
   } catch (err) {
     throw new Error(err);
