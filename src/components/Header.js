ff6db3d5737b9c544f5acbc52f726dc982d9a6d3
Daniel Wolter <wolter.daniel@gmail.com>
2022-10-28 14:00:59 -0300

    logo feita

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import RecipesContext from '../context/RecipesContext';
 import Search from './Search';
+import logo from '../img/T_DoceLogo.png';
 
 export default function Header() {
   const { tituloPagina, searchBar, showSearch,
@@ -14,7 +15,7 @@ export default function Header() {
     <header>
       <div className="header-icons">
         <div>
-          Main Group 8
+          <img className="imglogo-header" src={ logo } alt="Logo" />
         </div>
         <div className="header-profile-search">
           <button
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,31 @@
 import React, { useContext } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import RecipesContext from '../context/RecipesContext';
+import logo from '../img/T_DoceLogo.png';
 
 export default function Login() {
-  const { userEmail, password, useEmail, usePassword,
+  const { email, password, useEmail, usePassword,
     submitDisabled, submitInfo } = useContext(RecipesContext);
   return (
-    <form onSubmit={ submitInfo }>
-      <label htmlFor="email">
-        <input
+    <Form onSubmit={ submitInfo }>
+      <img className="imglogo" src={ logo } alt="Logo" />
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
           id="email"
           name="email"
           type="email"
           placeholder="Digite seu e-mail"
           data-testid="email-input"
-          value={ userEmail }
+          value={ email }
           onChange={ useEmail }
         />
-      </label>
-      <label htmlFor="password">
-        <input
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
           id="password"
           name="password"
           type="password"
@@ -27,14 +34,46 @@ export default function Login() {
           data-testid="password-input"
           onChange={ usePassword }
         />
-      </label>
-      <button
+      </Form.Group>
+      <Button
+        variant="primary"
         type="submit"
         data-testid="login-submit-btn"
         disabled={ submitDisabled }
       >
         Entrar
-      </button>
-    </form>
+      </Button>
+    </Form>
+    // <form onSubmit={ submitInfo }>
+    //   <label htmlFor="email">
+    //     <input
+    //       id="email"
+    //       name="email"
+    //       type="email"
+    //       placeholder="Digite seu e-mail"
+    //       data-testid="email-input"
+    //       value={ email }
+    //       onChange={ useEmail }
+    //     />
+    //   </label>
+    //   <label htmlFor="password">
+    //     <input
+    //       id="password"
+    //       name="password"
+    //       type="password"
+    //       value={ password }
+    //       placeholder="Digite sua senha"
+    //       data-testid="password-input"
+    //       onChange={ usePassword }
+    //     />
+    //   </label>
+    //   <button
+    //     type="submit"
+    //     data-testid="login-submit-btn"
+    //     disabled={ submitDisabled }
+    //   >
+    //     Entrar
+    //   </button>
+    // </form>
   );
 }
