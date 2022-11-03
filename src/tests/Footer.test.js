39ab83478d6f9446db04a8d7bd879f85f8254e07
devsakae <devsakae@gmail.com>
2022-11-03 17:18:59 -0300

    Testes

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
 import { renderWithRouter } from './renderWith';
 import RecipesProvider from '../context/RecipesProvider';
+// import App from '../App';
 import Meals from '../pages/Meals';
+import Drinks from '../pages/Drinks';
+// import meals from '../../cypress/mocks/meals';
 
 describe('Testes da tela Footer - Menu inferior', () => {
   it('Procura elementos obrigatórios', () => {
@@ -22,7 +26,7 @@ describe('Testes da tela Footer - Menu inferior', () => {
   it('Verifica se é possível ir para a rota /drinks', () => {
     const { history } = renderWithRouter(
       <RecipesProvider>
-        <Meals />
+        <Drinks />
       </RecipesProvider>,
     );
     if (history.location.pathname === '/meals') {
@@ -47,4 +51,23 @@ describe('Testes da tela Footer - Menu inferior', () => {
       expect(fetch).toHaveBeenCalled();
     }
   });
+  // it('Inicia uma nova receita', () => {
+  //   jest.spyOn(global, 'fetch').mockImplementation(async () => ({ json: async () => meals }));
+  //   const { history } = renderWithRouter(
+  //     <App />,
+  //   );
+  //   act(() => {
+  //     history.push('/meals');
+  //   });
+  //   if (history.location.pathname === '/meals') {
+  //     const firstCard = screen.getByTestId('0-recipe-card');
+  //     expect(firstCard).toBeInTheDocument();
+  //     userEvent.click(firstCard);
+  //     const startBtn = screen.findByTestId('start-recipe-btn');
+  //     expect(startBtn).toBeInTheDocument();
+  //     userEvent.click(startBtn);
+  //     const finishBtn = screen.findByTestId('finish-recipe-btn');
+  //     expect(finishBtn).toBeInTheDocument();
+  //   }
+  // });
 });
